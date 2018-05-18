0e4d209a218d51c015ac38c86e744e8ebbcf1b53
hlee398 <hlee398@PR18.hhs.win.fuhsd.org>
2018-05-18 11:33:02 -0700

    Added more stuff to menu

--- FinalProject/src/game/Menu.java
+++ FinalProject/src/game/Menu.java
@@ -1,29 +1,48 @@
 package game;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class Menu extends JPanel {
 
 	private JFrame frame;
-	
+	private String username;
 	private Game m;
 	
 	public Menu(Game m)
 	{
+		JTextField usernameArea = new JTextField();
+		usernameArea.addActionListener(new ActionListener()
+				{
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						username = usernameArea.getText();
+					}
+			
+				});
+		usernameArea.setEditable(true);
+		
+		BorderLayout b = new BorderLayout();
+		this.setLayout(b);
+		this.add(usernameArea, BorderLayout.EAST);
+		
 		this.m = m;
 		frame = new JFrame("Client Configuration");
 		frame.setBounds(0, 0, 400, 400);
 		frame.add(this);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
-		//JPanel p = new JPanel(new BorderLayout());
-		m.done();
+		//m.done();
 	}
 	
 	
 	public void done() {
+		
 		m.done();
 	}
+
 }
